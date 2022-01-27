8b32393881d242bb68da217029d156b753f2addb
oscar <oscartangplayz@gmail.com>
2022-01-28 01:55:59 +1000

    first commit

--- commands/balance.js
+++ commands/balance.js
@@ -3,9 +3,25 @@ module.exports = {
     aliases: ["bal", "bl"],
     description: "check user's balance",
     async execute(client, message, args, Discord, profileData){
+        const profileModel = require("../models/profileSchema");
+        var name;
+        if(!args.length){
+            var name = message.author.username;
+            var userData = profileData;
+        } else if(message.mentions.users.first()){
+            const target = message.mentions.users.first();
+            const targetData = await profileModel.findOne({ userID: target.id });
+            if (!targetData) return message.reply(`This user has to use a Jonathan Yun command first o.o`);
+            var name = target.username;
+            var userData = targetData
+        } else {
+            var name = message.author.username;
+            var userData = profileData;
+        }
+
         const embed = new Discord.MessageEmbed()
-        .setTitle(`**${message.author.username}'s YunBuck Balance**`)
-        .setDescription(`**Wallet:** ¥${profileData.coins}\n**Bank:** ¥${profileData.bank}`)
+        .setTitle(`**${name}'s YunBuck Balance**`)
+        .setDescription(`**Wallet:** ¥${userData.coins}\n**Bank:** ¥${userData.bank}`)
         .setColor('GREEN')
 
         await message.reply({
