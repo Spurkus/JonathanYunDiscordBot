8dc5ce7d8555ace9d063cb8e5cb618581eb19136
oscar <oscartangplayz@gmail.com>
2022-01-27 21:14:18 +1000

    first commit

--- commands/balance.js
+++ commands/balance.js
@@ -4,8 +4,8 @@ module.exports = {
     description: "check user's balance",
     async execute(client, message, args, Discord, profileData){
         const embed = new Discord.MessageEmbed()
-        .setTitle('Balance')
-        .setDescription(`Wallet: ${profileData.coins}\nBank: ${profileData.bank}`)
+        .setTitle(`**${Message.author}'s YunBuck Balance**`)
+        .setDescription(`**Wallet:** ¥${profileData.coins}\n**Bank:** ¥${profileData.bank}`)
         .setColor('GREEN')
 
         await message.reply({
--- commands/beg.js
+++ commands/beg.js
@@ -4,18 +4,21 @@ module.exports = {
     aliases: [],
     description: "beg for coins",
     async execute(client, message, args, Discord, profileData){
-        const randomNumber = Math.floor(Math.random() * 500) + 1;
-        const response = await profileModel.findOneAndUpdate(
-            {
-                userID: message.author.id,
-            },
-            {
-                $inc: {
-                    coins: randomNumber,
-                },
-            }
-        );
-    return message.reply(`${message.author.username}, you begged and received ${randomNumber} **coins**`);
+        const randomChance = Math.floor(Math.random() * 100);
+        console.log(randomChance)
+        var randomNumber;
+        if(randomChance <= 15){
+            var randomNumber = 0;
+            message.reply(`${message.author.username}, you begged and nobody likes you so recieved nothing LMAOO. ¥${randomNumber} **YunBucks** was added`);
+        } else if(randomChance <= 25){
+            var randomNumber = -Math.floor(Math.random() * 700) + 1;
+            var pos = randomNumber * -1;
+            message.reply(`${message.author.username}, you begged and someone hates you so they decided to rob you L. ¥${pos} **YunBucks** was taken from your wallet`);
+        } else {
+            var randomNumber = Math.floor(Math.random() * 500) + 1;
+            message.reply(`${message.author.username}, you begged and recieved ¥${randomNumber} **YunBucks**.`);
+        }
+        console.log(randomNumber)
   },
 };
 
