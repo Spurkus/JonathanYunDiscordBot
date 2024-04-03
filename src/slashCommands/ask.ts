db491a4a52ff413b0241a1fb06672b20e721e9ed
Spurk <89559431+Spurkus@users.noreply.github.com>
2024-04-04 00:04:45 +1100

    Update ask.ts

--- src/slashCommands/ask.ts
+++ src/slashCommands/ask.ts
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 import { SlashCommandBuilder } from "discord.js";
-import { SlashCommand } from "../types";
+import { SlashCommand } from "../utility/types";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
